1b3a7194c8c6627f22c7f35d38adb4be5bc18f0f
P3ANTMC <pean.tmc@seznam.cz>
2024-01-09 19:11:51 +0100

    Greeting and user inputs

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -6,4 +6,6 @@ bulls_and_cows.py: Second Engeto Online Academy project
  discord: PETR H.
 """
 
-
+print(f"Hi there!")
+name_input = input("What is your name?")
+print(f"Let's play a game BULLS AND COWS {name_input.capitalize()}")
